99c49071b08894584a05a6092b09a5f3d27b41f6
rcasamajo <rcasamajo@iespoblenou.org>
2015-12-17 17:05:28 +0100

    En procés

--- src/zoom/Controller.java
+++ src/zoom/Controller.java
@@ -1,5 +1,6 @@
 package zoom;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
@@ -14,8 +15,18 @@ public class Controller {
     @FXML
     private ImageView iView;
 
-
     public void initialize() {
         iView.setImage(new Image("Homer_Simpson_2006.png"));
-    }
+        iView.setFitWidth(250D);
+        iView.setFitHeight(250D);
+
+        sliderZoom.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                                Number oldValue, Number newValue) {
+
+                outputTextArea.appendText("Slider Value Changed (newValue: " + newValue.intValue() + ")\n");
+            }
+        });    }
+
 }
